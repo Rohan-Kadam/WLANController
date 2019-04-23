17f94b8d915124465f766eec8c46a0fd97f2ab32
Rohan-Kadam <rkadam202@gmail.com>
2019-04-23 16:51:46 +0530

    <feat>: Initial version of TransAPI Modules for both netopeer servers
    The only difference between the two modules is pipe name for sending data to AP/neto-controller
    and notification file name for receiving data from AP/neto-controller.
    
    TODO: The YANG Model parameter handling is limited. Need to extend it.
    TODO: No packaging mechanism used, before sending data over pipe. (Use protobuf-c or binn library)

---
+++ auto/netconf/MW-netopeer/transAPI/test/test_bkp.c
@@ -0,0 +1,222 @@
+/*
+ * This is automatically generated callbacks file
+ * It contains 3 parts: Configuration callbacks, RPC callbacks and state data callbacks.
+ * Do NOT alter function signatures or any structures unless you know exactly what you are doing.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>	
+#include <sys/inotify.h>
+#include <libxml/tree.h>
+#include <libnetconf_xml.h>
+#include <sys/shm.h>
+#include <sys/stat.h>
+
+
+/* transAPI version which must be compatible with libnetconf */
+int transapi_version = 6;
+
+/* Signal to libnetconf that configuration data were modified by any callback.
+ * 0 - data not modified
+ * 1 - data have been modified
+ */
+int config_modified = 0;
+
+/*
+ * Determines the callbacks order.
+ * Set this variable before compilation and DO NOT modify it in runtime.
+ * TRANSAPI_CLBCKS_LEAF_TO_ROOT (default)
+ * TRANSAPI_CLBCKS_ROOT_TO_LEAF
+ */
+const TRANSAPI_CLBCKS_ORDER_TYPE callbacks_order = TRANSAPI_CLBCKS_ORDER_DEFAULT;
+
+/* Do not modify or set! This variable is set by libnetconf to announce edit-config's error-option
+Feel free to use it to distinguish module behavior for different error-option values.
+ * Possible values:
+ * NC_EDIT_ERROPT_STOP - Following callback after failure are not executed, all successful callbacks executed till
+                         failure point must be applied to the device.
+ * NC_EDIT_ERROPT_CONT - Failed callbacks are skipped, but all callbacks needed to apply configuration changes are executed
+ * NC_EDIT_ERROPT_ROLLBACK - After failure, following callbacks are not executed, but previous successful callbacks are
+                         executed again with previous configuration data to roll it back.
+ */
+NC_EDIT_ERROPT_TYPE erropt = NC_EDIT_ERROPT_NOTSET;
+
+/**
+ * @brief Initialize plugin after loaded and before any other functions are called.
+
+ * This function should not apply any configuration data to the controlled device. If no
+ * running is returned (it stays *NULL), complete startup configuration is consequently
+ * applied via module callbacks. When a running configuration is returned, libnetconf
+ * then applies (via module's callbacks) only the startup configuration data that
+ * differ from the returned running configuration data.
+
+ * Please note, that copying startup data to the running is performed only after the
+ * libnetconf's system-wide close - see nc_close() function documentation for more
+ * information.
+
+ * @param[out] running	Current configuration of managed device.
+
+ * @return EXIT_SUCCESS or EXIT_FAILURE
+ */
+int transapi_init(xmlDocPtr *running) {
+	return EXIT_SUCCESS;
+}
+
+/**
+ * @brief Free all resources allocated on plugin runtime and prepare plugin for removal.
+ */
+void transapi_close(void) {
+	return;
+}
+
+/**
+ * @brief Retrieve state data from device and return them as XML document
+ *
+ * @param model	Device data model. libxml2 xmlDocPtr.
+ * @param running	Running datastore content. libxml2 xmlDocPtr.
+ * @param[out] err  Double pointer to error structure. Fill error when some occurs.
+ * @return State data as libxml2 xmlDocPtr or NULL in case of error.
+ */
+xmlDocPtr get_state_data(xmlDocPtr model, xmlDocPtr running, struct nc_err **err) {
+	return(NULL);
+}
+/*
+ * Mapping prefixes with namespaces.
+ * Do NOT modify this structure!
+ */
+struct ns_pair namespace_mapping[] = {{"test", "http://example.net/test"}, {NULL, NULL}};
+
+/*
+ * CONFIGURATION callbacks
+ * Here follows set of callback functions run every time some change in associated part of running datastore occurs.
+ * You can safely modify the bodies of all function as well as add new functions for better lucidity of code.
+ */
+
+/**
+ * @brief This callback will be run when node in path /test:test/test:ap_id changes
+ *
+ * @param[in] data	Double pointer to void. Its passed to every callback. You can share data using it.
+ * @param[in] op	Observed change in path. XMLDIFF_OP type.
+ * @param[in] old_node	Old configuration node. If op == XMLDIFF_ADD, it is NULL.
+ * @param[in] new_node	New configuration node. if op == XMLDIFF_REM, it is NULL.
+ * @param[out] error	If callback fails, it can return libnetconf error structure with a failure description.
+ *
+ * @return EXIT_SUCCESS or EXIT_FAILURE
+ */
+/* !DO NOT ALTER FUNCTION SIGNATURE! */
+int callback_test_test_test_ap_id(void **data, XMLDIFF_OP op, xmlNodePtr old_node, xmlNodePtr new_node, struct nc_err **error) {
+
+	if (op & (XMLDIFF_ADD | XMLDIFF_MOD)) {
+
+		char* tmp1,*tmp2;
+		tmp1 = new_node->children->content;	
+		tmp2 = new_node->next->children->content;
+
+		key_t shm_key = 6166529;
+		const int shm_size = 1024;
+
+		int shm_id;
+		int *shmaddr;
+
+		shm_id = shmget (shm_key, shm_size, IPC_CREAT | S_IRUSR | S_IWUSR);
+		shmaddr = (int*) shmat (shm_id, 0, 0);
+
+		shmaddr[0] = tmp1[0] - '0';
+		shmaddr[1] = tmp2[0] - '0';
+	}
+	return EXIT_SUCCESS;
+}
+
+/*
+ * Structure transapi_config_callbacks provide mapping between callback and path in configuration datastore.
+ * It is used by libnetconf library to decide which callbacks will be run.
+ * DO NOT alter this structure
+ */
+struct transapi_data_callbacks clbks =  {
+	.callbacks_count = 1,
+	.data = NULL,
+	.callbacks = {
+		{.path = "/test:test/test:ap_id", .func = callback_test_test_test_ap_id}
+	}
+};
+
+/**
+ * @brief Get a node from the RPC input. The first found node is returned, so if traversing lists,
+ * call repeatedly with result->next as the node argument.
+ *
+ * @param name	Name of the node to be retrieved.
+ * @param node	List of nodes that will be searched.
+ * @return Pointer to the matching node or NULL
+ */
+xmlNodePtr get_rpc_node(const char *name, const xmlNodePtr node) {
+	xmlNodePtr ret = NULL;
+
+	for (ret = node; ret != NULL; ret = ret->next) {
+		if (xmlStrEqual(BAD_CAST name, ret->name)) {
+			break;
+		}
+	}
+
+	return ret;
+}
+
+/*
+ * RPC callbacks
+ * Here follows set of callback functions run every time RPC specific for this device arrives.
+ * You can safely modify the bodies of all function as well as add new functions for better lucidity of code.
+ * Every function takes an libxml2 list of inputs as an argument.
+ * If input was not set in RPC message argument is set to NULL. To retrieve each argument, preferably use get_rpc_node().
+ */
+
+/*
+ * Structure transapi_rpc_callbacks provides mapping between callbacks and RPC messages.
+ * It is used by libnetconf library to decide which callbacks will be run when RPC arrives.
+ * DO NOT alter this structure
+ */
+struct transapi_rpc_callbacks rpc_clbks = {
+	.callbacks_count = 0,
+	.callbacks = {
+	}
+};
+
+int example_callback1(const char *filepath, xmlDocPtr *edit_config, int *exec) {
+      // do the job with changed file content
+      // if needed, set edit_config parameter to the edit-config data to be applied
+      // if needed, set exec to 1 to perform consequent transapi callbacks
+
+	FILE *fp;
+	long lSize;
+	char *buffer;
+	char* str;
+
+	fp = fopen ( "/etc/my_cfg_file.txt" , "rb" );
+	if( !fp ) perror("/etc/my_cfg_file1.txt"),exit(1);
+
+	fseek(fp,0L,SEEK_END);
+	lSize = ftell( fp );
+	rewind( fp );
+
+	/* allocate memory for entire content */
+	buffer = calloc( 1, lSize+1 );
+	if( !buffer ) fclose(fp),fputs("memory alloc fails",stderr),exit(1);
+
+	/* copy the file into the buffer */
+	if( 1!=fread( buffer , lSize, 1 , fp) )
+	fclose(fp),free(buffer),fputs("entire read fails",stderr),exit(1);
+
+	asprintf(&str, "<event>%s</event>", buffer);
+	ncntf_event_new(-1, NCNTF_GENERIC,str);
+
+	fclose(fp);
+	free(buffer);
+
+    return 0;
+	}
+
+ 
+  struct transapi_file_callbacks file_clbks = {
+      .callbacks_count = 1,
+      .callbacks = {
+          {.path = "/etc/my_cfg_file.txt", .func = example_callback1}
+      }
+  };
